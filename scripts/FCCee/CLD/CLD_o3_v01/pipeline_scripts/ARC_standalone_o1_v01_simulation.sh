0d53d63d37de658b1332d3c4b95a9926fc002bf5
Enrico Lupi <enrico.lupi@cern.ch>
2024-08-06 10:45:33 +0200

    Test fro simulation angular distribution

--- scripts/FCCee/CLD/CLD_o3_v01/pipeline_scripts/ARC_standalone_o1_v01_simulation.sh
+++ scripts/FCCee/CLD/CLD_o3_v01/pipeline_scripts/ARC_standalone_o1_v01_simulation.sh
@@ -6,10 +6,10 @@ echo "Sourcing executed successfully"
 cd CLDConfig/CLDConfig
 echo "Starting simulation..."
 ddsim --steeringFile cld_arc_steer.py \
-      --enableGun --gun.distribution "cos(theta)" \ 
+      --enableGun --gun.distribution uniform \ 
       --gun.energy "20*GeV" --gun.particle proton \
       --numberOfEvents $NUMBER_OF_EVENTS \
-      --outputFile $WORKAREA/$GEOMETRY/$VERSION/ARC_sim.root \
+     --outputFile  $WORKAREA/$GEOMETRY/$VERSION/ARC_sim.root \
       --random.enableEventSeed --random.seed 42
 echo "Simulation ended successfully"
 cd $WORKAREA/$GEOMETRY/$VERSION
